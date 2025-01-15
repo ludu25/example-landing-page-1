5da8798d5c56b2ba122f3e8adb76a1ab08747cde
Lucas Ventavele Ribeiro <lucasventavele@hotmail.com>
2025-01-15 15:32:11 -0300

    Fix app head tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,15 +3,18 @@ import "../styles/custom.css";
 import type { AppProps } from "next/app";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import Head from "next/head";
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta name="author" content="Lucas Ventavele Ribeiro">
-      <meta name="description" content="Evento que acontecerá para programadores com convidados especiais">
-      <title>SIEGEPAN</title>
+      <Head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="author" content="Lucas Ventavele Ribeiro" />
+        <meta name="description" content="Evento que acontecerá para programadores com convidados especiais" />
+        <title>SIEGEPAN</title>
+      </Head>
 
       <Header />
 
